cca91b58a2620c95d3e14aa49f533497770d67fb
PurpleEyeGH <oeilpourprekebab@gmail.com>
2020-11-23 15:37:12 +0100

    Add Helmet

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as helmet from 'helmet';
 
 declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(helmet());
   await app.listen(3000);
   if (module.hot) {
     module.hot.accept();
